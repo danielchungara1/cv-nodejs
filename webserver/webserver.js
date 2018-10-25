7b363854b6022bac28824ab385c6570768428d5a
daniel chungara <danielchungara1@gmail.com>
2018-10-25 12:57:07 +0100

    respondiendo con index.html

--- webserver/webserver.js
+++ webserver/webserver.js
@@ -1,4 +1,5 @@
 var modulehttp = require('http');
+var modulefs = require ('fs');
 
 //Helpers
 var port = process.env.PORT || 5000;
@@ -6,8 +7,14 @@ console.log('port available to listen ' + port);
 
 modulehttp.createServer(function(req, res){
     if(req.url=="/" || req.url=="/index.html") {   
-        return res.end("deberia devolver el index.html")
+        modulefs.readFile("public/index.html",function(err, data){
+            if (err) throw err;
+            res.writeHead(200,{'Content-Type':'text/html'});
+            res.write(data);
+            return res.end();
+        })        
     }else{
-        return res.end("404")
+        res.writeHead(404,{'Content-Type':'text/html'});
+        res.end("RECURSO NO ENCONTRADO");
     }      
 }).listen(port);
