7b64e5d928a94a3183759f6dd5de65d49c779e5d
daniel chungara <danielchungara1@gmail.com>
2018-10-25 17:52:18 +0100

    header nav

--- webserver/webserver.js
+++ webserver/webserver.js
@@ -3,31 +3,18 @@ var modulefs = require ('fs');
 
 //Helpers
 var port = process.env.PORT || 5000;
-console.log('port available to listen ' + port);
+
 
 modulehttp.createServer(function(req, res){
     if(req.url=="/" || req.url=="/index.html") {   
-        modulefs.readFile("public/index.html",function(err, data){
+        modulefs.readFile("index.html",function(err, data){
             if (err) throw err;
             res.writeHead(200,{'Content-Type':'text/html'});
             res.write(data);
             return res.end();
         })        
-    }else if(req.url=="/public/css/estilos-index.css"){
-        modulefs.readFile("public/css/estilos-index.css",function(err, data){
-            if (err) throw err;
-            res.writeHead(200,{'Content-Type':'text/css'});
-            res.write(data);
-            return res.end();
-        })
-    }else if(req.url=="/public/img/banner/cv-lupa.jpg"){
-        modulefs.readFile("public/img/banner/cv-lupa.jpg",function(err, data){
-            if (err) throw err;
-            res.writeHead(200,{'Content-Type':'text/image/jpg'});
-            res.write(data);
-            return res.end();
-        })
-    }else if(req.url=="/favicon.ico"){
+    }
+    else if(req.url=="/favicon.ico"){
         modulefs.readFile("public/img/logo.ico",function(err, data){
             if (err) throw err;
             res.writeHead(200,{'Content-Type':'image/ico'});
@@ -36,8 +23,18 @@ modulehttp.createServer(function(req, res){
         })
     }        
     else{
-        res.writeHead(404,{'Content-Type':'text/html'});
-        res.end("RECURSO NO ENCONTRADO: " + req.url);
-        console.log("RECURSO NO ENCONTRADO" + req.url);
-    }      
-}).listen(port);
+        let patharchivo = req.url.slice(1);                
+        modulefs.readFile(patharchivo, function(err, data){
+            if (err){
+                res.writeHead(400);
+                return res.end("RECURSO NO ENCONTRADO");
+            };            
+            res.writeHead(200);
+            res.write(data);
+            return res.end();
+        })
+    }
+    
+}).listen(port, function(){
+    console.log('server listening port ' + port);
+});
