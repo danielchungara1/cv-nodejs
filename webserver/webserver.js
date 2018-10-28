5266d963031027eb55bdf94a132f1c5cba19499d
daniel chungara <danielchungara1@gmail.com>
2018-10-28 20:49:35 +0100

    agregando mas paginas experiencia, idiomas, etc...

--- webserver/webserver.js
+++ webserver/webserver.js
@@ -7,7 +7,7 @@ var port = process.env.PORT || 5000;
 
 modulehttp.createServer(function(req, res){
     if(req.url=="/" || req.url=="/index.html") {   
-        modulefs.readFile("public/html/index.html",function(err, data){
+        modulefs.readFile("index.html",function(err, data){
             if (err) throw err;
             res.writeHead(200,{'Content-Type':'text/html'});
             res.write(data);
