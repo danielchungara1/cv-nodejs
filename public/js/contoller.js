ac7522571ab1fdbf38aa4e1e06b197bd4da4059a
daniel chungara <danielchungara1@gmail.com>
2018-11-08 22:50:15 -0300

    agregando modulo ng-route

--- public/js/contoller.js
+++ public/js/contoller.js
@@ -1,14 +1,42 @@
-var app =angular.module("app",[]);
+var app =angular.module("app",["ngRoute"]);
 
-app.config(function($sceProvider) {
+/* app.config(function($sceProvider) {
     // Completely disable SCE.  For demonstration purposes only!
     // Do not use in new projects or libraries.
     $sceProvider.enabled(false);
   });
+ */
+  app.config(function($routeProvider) {
+    $routeProvider
+    .when("/", {
+        templateUrl : "/public/html/index-main.html"
+    })
+    .when("/personal", {
+        templateUrl : "/public/html/personal-main.html"
+    })
+    .when("/educacion", {
+        templateUrl : "/public/html/educacion-main.html"
+    })
+    .when("/experiencia", {
+        templateUrl : "/public/html/experiencia-main.html"
+    })
+    .when("/idiomas", {
+        templateUrl : "/public/html/idiomas-main.html"
+    })
+    .when("/herramientas", {
+        templateUrl : "/public/html/herramientas-main.html"
+    })
+    .when("/objetivos", {
+        templateUrl : "/public/html/objetivos-main.html"
+    })
+    .when("/projectos", {
+        templateUrl : "/public/html/projectos-main.html"
+    });
+});
 
 app.controller("controller", function($scope, $http){
     
-    $http.get("/public/html/index-main.html").then(
+/*     $http.get("/public/html/index-main.html").then(
         function(response){
             $scope.mainhtml = response.data;
         }
@@ -20,6 +48,7 @@ app.controller("controller", function($scope, $http){
             $scope.mainhtml = response.data;
         }
     );
-   }
+   } */
+
 })
 
